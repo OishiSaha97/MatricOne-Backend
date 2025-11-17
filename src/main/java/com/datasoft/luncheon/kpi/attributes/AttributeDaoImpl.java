ef427bdd9ccb94e295ff0557ae1a57547f1b6478
kaungkon <mallickkaungkon@gmail.com>
2025-11-17 16:29:53 +0600

    new branch

--- src/main/java/com/datasoft/luncheon/kpi/attributes/AttributeDaoImpl.java
+++ src/main/java/com/datasoft/luncheon/kpi/attributes/AttributeDaoImpl.java
@@ -13,6 +13,18 @@ public class AttributeDaoImpl implements AttributeDao{
 
     @Override
     public void save(String selectedKpiType,String attributeName,String userId) {
+        String checkSql = "SELECT COUNT(*) FROM conf_kpi_attribute " +
+                "WHERE kpi_type = ? AND kpi_category_name = ?";
+
+        Integer count = jdbcTemplate.queryForObject(
+                checkSql,
+                new Object[]{selectedKpiType, attributeName},
+                Integer.class
+        );
+        if (count != null && count > 0) {
+            return;
+        }
+
         String sql = "INSERT INTO conf_kpi_attribute (kpi_type, kpi_category_name, status,created_by, created_at) VALUES (?, ?, ?, ?, NOW())";
         jdbcTemplate.update(sql, selectedKpiType, attributeName,1, userId);
     }
