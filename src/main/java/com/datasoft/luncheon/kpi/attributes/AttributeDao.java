a3654545765f623dd99547f9c1fbfcf86f066bef
kaungkon <mallickkaungkon@gmail.com>
2025-11-18 15:49:20 +0600

    new branch

--- src/main/java/com/datasoft/luncheon/kpi/attributes/AttributeDao.java
+++ src/main/java/com/datasoft/luncheon/kpi/attributes/AttributeDao.java
@@ -1,9 +1,8 @@
 package com.datasoft.luncheon.kpi.attributes;
 
-import com.datasoft.luncheon.kpi.KpiConfigParams;
-import com.datasoft.luncheon.kpi.attributes.dto.AttributeDto;
+import com.datasoft.luncheon.commons.model.ApiResponse;
 
 public interface AttributeDao {
 
-    void save(String selectedKpiType,String attributeName,String userId);
+    ApiResponse save(String selectedKpiType, String attributeName, String userId);
 }
